f1b1753f76fcb5b0528c563db5f67c7c188a179f
Sanxicson C Benjamin <sanxicson@gmail.com>
2020-11-30 22:42:59 +0530

    Implement Advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,8 +9,21 @@ class ActiveSupport::TestCase
   fixtures :all
   #Returns true if a test user is logged in.
   include ApplicationHelper
+
   def is_logged_in?
   	!session[:user_id].nil?
   end
+
+  def log_in_as(user)
+  	session[:user_id] = user.id
+  end
+
+end
+
+class ActionDispatch::IntegrationTest
+
+	def log_in_as(user, password: 'password', remember_me: 1)
+		post '/login', params: {session: {email: user.email, password: password, remember_me: remember_me } }
+	end
 
 end
